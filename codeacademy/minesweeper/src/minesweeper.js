c3127fdcc074bf54c4b40574cd109a193c22b120
TheAwesomeSimon <34960705+TheAwesomeSimon@users.noreply.github.com>
2018-04-11 23:35:58 +0200

    minesweeper 2.0

--- codeacademy/minesweeper/src/minesweeper.js
+++ codeacademy/minesweeper/src/minesweeper.js
@@ -10,4 +10,37 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     return board;
 };
 
-console.log(generatePlayerBoard(3,3));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    var board = [];
+    for (let row = 0; row < numberOfRows; row++) {
+        rows = [];
+        for (let column = 0; column < numberOfColumns; column++) {
+            rows.push(null);
+        };
+        board.push(rows);
+    };
+    let numberOfBombsPlaced = 0;
+    while (numberOfBombsPlaced < numberOfBombs) {
+        //possible to place bomb on top of bomb
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        if (board[randomRowIndex][randomColumnIndex] != 'B') {
+            board[randomRowIndex][randomColumnIndex] = 'B';
+            numberOfBombsPlaced++;
+        } 
+    };
+    
+    return board;
+};
+
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+printBoard(bombBoard);
